65c8c76baf560e6c6c90e00b0e7bdef4f70f7754
tomlaz <tomaslazarte@gmail.com>
2022-12-05 23:30:42 -0300

    first commit

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,10 @@
+import {pool} from '../db.js';
+
+export const ping = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT 'Pong' AS result");
+    res.json(result[0]);
+  } catch (error) {
+    console.log( 'Something went wrong:', error );
+  }
+};
